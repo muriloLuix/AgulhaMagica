17d3ca96f15ea06b52f0f9ebcd4fe13f1307bdec
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2023-12-07 15:24:14 -0300

    JavaScript Menu and Responsive Page

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-alrt("Esse site ainda está em desenvolvimento pelo criador!")
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
 
-// window.prompt
-// window.alert
-// document.write (Irá escrever no bg atrás)
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('open-menu');
+    overlay.style.display = 'block';
+});
+
+function closeMenu() {
+    if (menu.classList.contains('open-menu')) {
+        menu.classList.remove('open-menu');
+        overlay.style.display = 'none'; 
+    }
+}
+
+menu.addEventListener('click', () => {
+    closeMenu();
+});
+
+overlay.addEventListener('click', () => {
+    closeMenu();
+});
